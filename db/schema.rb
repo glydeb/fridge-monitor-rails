3a5f60c94da33fff3099e2899ea76ed663ff7452
Glydewell Burdick <glydewellb@gmail.com>
2023-05-21 16:28:47 -0500

    new schema after fridge migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_05_04_045213) do
+ActiveRecord::Schema[7.1].define(version: 2023_05_21_210529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "fridges", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.boolean "has_refrig"
+    t.boolean "has_freezer"
+    t.boolean "is_healthy"
+    t.datetime "last_alerted"
+    t.datetime "last_reported"
+    t.integer "report_frequency"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_fridges_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +43,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_05_04_045213) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "fridges", "users"
 end
